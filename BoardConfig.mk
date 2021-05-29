f9c355706cc54e63a8fd40e12a012383edaf92a0
Sarthak <sarthakroy2002@gmail.com>
2021-05-30 00:00:52 +0530

    RMX2193: Remove TARGET_LD_SHIM_LIBS
    Change-Id: I7489eab438c9cca9c89b642b2febda70a9decd15

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,9 +131,6 @@ TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
-# Symbols
-TARGET_LD_SHIM_LIBS := /system/lib/libshowlogo.so|libshim_showlogo.so
-
 # Treble
 TARGET_COPY_OUT_PRODUCT := system/product
 
